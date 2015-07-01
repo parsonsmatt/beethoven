d226eb8066623c109038e305e4ec0f35bad78ba8
Matt Parsons <parsonsmatt@gmail.com>
2015-07-01 19:14:17 -0400

    refinements

--- lib/beethoven.rb
+++ lib/beethoven.rb
@@ -1,7 +1,25 @@
 require "beethoven/version"
 require "beethoven/composer"
 require "beethoven/composable"
-require "beethoven/class"
 
 module Beethoven
+  refine Class do
+    def *(other)
+      Beethoven::Composer.new(other, self)
+    end
+
+    def |(other)
+      Beethoven::Composer.new(self, other)
+    end
+  end
+
+  refine Object do
+    def *(other)
+
+    end
+
+    def |(other)
+
+    end
+  end
 end
